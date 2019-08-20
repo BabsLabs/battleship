e80821bbb1f67ead48bfda9e3fe581f9457047b8
Brian Bower <brian@brianbower.com>
2019-08-19 23:10:24 -0600

    Add code comments

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,22 +1,30 @@
+# Make Ship class
 class Ship
+  # Ship attributes
   attr_reader :name,
               :length,
               :health
 
+  # Initialization method
   def initialize(name, length)
+    # Instance variables
     @name = name
     @length = length
     @health = length
   end
 
+  # sunk? method
   def sunk?
+    # sinks ship when health is zero
     if @health == 0
+      # returns bool
       true
     else
       false
     end
   end
 
+  # hit method for -1 health
   def hit
     @health -= 1
   end
