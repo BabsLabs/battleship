7e19fcfa9f9579c30c5c047f04b13dad6c703e20
Brian Bower <brian@brianbower.com>
2019-08-20 12:49:24 -0600

    (blank line) (blank line) Co-authored-by: David Barriga-Gomez <50917634+DavidBarriga-Gomez@users.noreply.github.com>
    "Add empty? method and test"

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -7,11 +7,27 @@ class CellTest < Minitest::Test
 
   def setup
     @cell = Cell.new("B4")
+    @cruiser = Ship.new("Cruiser", 3)
   end
 
   def test_it_exists
     assert_instance_of Cell, @cell
   end
 
+  def test_coordinate
+    assert_equal "B4", @cell.coordinate
+  end
+
+  def test_cell_does_not_have_a_ship
+    assert_equal nil, @cell.ship
+  end
+
+  def test_empty?
+    assert_equal true, @cell.empty?
+  end
+
+  def test_cruiser_class
+    assert_instance_of Ship, @cruiser
+  end
 
 end
