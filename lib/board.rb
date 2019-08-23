53138745b23ba4508fb468d451d728b64b7d9aab
Brian Bower <brian@brianbower.com>
2019-08-23 12:00:22 -0600

    Add working valid placement and consecutive methods and board_tests
    Co-authored-by: David Barriga-Gomez <50917634+DavidBarriga-Gomez@users.noreply.github.com>

--- lib/board.rb
+++ lib/board.rb
@@ -1,70 +1,3 @@
-<<<<<<< HEAD
-class Board
-  attr_reader :cells
-
-  def initialize
-    @cells = {
-      "A1" => Cell.new("A1"),
-      "A2" => Cell.new("A2"),
-      "A3" => Cell.new("A3"),
-      "A4" => Cell.new("A4"),
-      "B1" => Cell.new("B1"),
-      "B2" => Cell.new("B2"),
-      "B3" => Cell.new("B3"),
-      "B4" => Cell.new("B4"),
-      "C1" => Cell.new("C1"),
-      "C2" => Cell.new("C2"),
-      "C3" => Cell.new("C3"),
-      "C4" => Cell.new("C4"),
-      "D1" => Cell.new("D1"),
-      "D2" => Cell.new("D2"),
-      "D3" => Cell.new("D3"),
-      "D4" => Cell.new("D4"),
-    }
-  end
-
-  def valid_coordinate?(coordinate)
-    # check if the coordinate is one of the cell keys
-    @cells.key?(coordinate)
-  end
-
-  def valid_placement?(ship_object, coordinate_array)
-    # check to see if coordinate_array is the length of the ship
-    # if ship_object.length !=  coordinate_array.length
-    #   false
-
-    # check if the coordinate_array is consecutive
-    letters_array = []
-    numbers_array = []
-
-    # iterate through coordinate array and split the coordinate into to arrays. A letter array and a number array.
-    coordinate_array.each do |coordinate|
-      letters_array << coordinate.split('').first
-      numbers_array << coordinate.split('').last
-    end
-
-    # check that all the letters in the letters_array are the same
-    # if letters_array.all? {|letter| letter == letters_array[0]}
-    # end
-
-    # check all the numbers in the numbers_array are consecutive using ordinal values
-    ord_array = []
-    numbers_array.each do |number|
-      ord_array << number.ord
-    end
-
-    if ord_array.sum != (ord_array[0] * (ord_array.length) + ord_array.length)
-      false
-    else
-      true
-    end
-
-    # end of first if statement on length
-    #   true
-    # end
-  end
-
-=======
 require 'pry'
 class Board
   attr_reader :cells
@@ -90,76 +23,34 @@ class Board
                   }
   end
 
+
   def valid_coordinate?(coordinate)
     @cells.include? coordinate
-    #make enumerable verify both key && value == coordinate
-    # found_coordinate = nil
-    # @cells.each do |key, value|
-    #   if value.coordinate == coordinate
-    #      found_coordinate = true
-    #     # binding.pry
-    #     break
-    #   elsif value.coordinate != coordinate
-    #        found_coordinate = false
-    #     break
-    #   end
-    # end
-    # found_coordinate
   end
 
-  # def valid_placement?(ship, coordinates)
-    # # if ship.length != coordinates.length
-    # #   false
-    # # end
-    # letters_array = []
-    # numbers_array = []
-    # coordinates.each do |coordinate|
-    #   # coordinate.split("").first && coordinate.split("").last
-    #   letters_array.push coordinate.split("").first
-    #   numbers_array.push coordinate.split("").last
-    #   if
-    #   letters_array.all? do |letter|
-    #     letter == letters_array[0]
-    #
-    #   end
-    #   numbers_array.each_cons(3) do |number|
-    #     number + 1 ==
-    #     binding.pry
-  #     end
-  #     end
-  #   end
-  #
 
-#==================================
-
-  # end
-
-  def consecutive?(ship, array_coordinates)
-
-    if ship.length == array_coordinates.length
-      true
-    else
+  def valid_placement?(ship, coordinates)
+    if ship.length != coordinates.length
       false
     end
-    row_values = []
-    column_values = []
-    array = array_coordinates.map do |coordinate|
-      single_cord = coordinate.split("")
-      row_values.push single_cord[0]
-      column_values.push single_cord[1].to_i
-      # coordinate.ord + coordinate[1].to_i
-    end
-    row_values.uniq == 1
-    column_values.each_cons(2) do |x, y|
-
-  # print x
-  # print y
-      binding.pry
-
-    end
-
-  #   array.sort.each_cons(2).all? { |x, y| x == y - 1 }
-  #   binding.pry
   end
->>>>>>> 35cb8c6f559f53aa3ca99e587760735abf3eea57
+
+
+  def consecutive?(ship, array_coordinates)
+    row_values = [] # empty array for letter
+    column_values = [] #  empty array for numbers
+    array = array_coordinates.map do |coordinate| # iterate through array_coordinates
+      single_cord = coordinate.split("") # splits every coordinate into arrays of two elements
+      row_values.push single_cord[0] # add letters to row coordinate array
+      column_values.push single_cord[1].to_i # add numbers to column array
+      coordinate.ord + coordinate[1].to_i # this is what gets mapped to the array - the ordinal values
+    end
+    # row_values.uniq == 1 # check to make sure all letters are the same (maybe useless?)
+    array.sort.each_cons(2).all? { |x, y| x == y - 1 } # makes array pairs from our array and checks that they are consecutive
+    # using the ord values and checking for x == y - 1 prevents diagnol placement
+  end
+
+
+
+
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -1,37 +1,17 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-<<<<<<< HEAD
-require './lib/board'
-require './lib/cell'
-require './lib/ship'
-require 'pry'
-
-class BoardTest < Minitest::Test
-
-=======
 require './lib/ship'
 require './lib/cell'
 require './lib/board'
 require 'pry'
 
 class CellTest < Minitest::Test
->>>>>>> 35cb8c6f559f53aa3ca99e587760735abf3eea57
   def setup
     @board = Board.new
     @cruiser = Ship.new("Cruiser", 3)
     @submarine = Ship.new("Submarine", 2)
   end
 
-<<<<<<< HEAD
-  def test_it_exists
-    assert_instance_of Board, @board
-  end
-
-  def test_cells
-    assert_equal 16, @board.cells.keys.length
-    assert_equal 16, @board.cells.values.length
-    # verify that the values are cell objects
-=======
   def test_existence
     assert_instance_of Board, @board
   end
@@ -39,7 +19,6 @@ class CellTest < Minitest::Test
   def test_board_cells
     assert_equal 16, @board.cells.keys.length
     assert_equal 16, @board.cells.values.length
->>>>>>> 35cb8c6f559f53aa3ca99e587760735abf3eea57
   end
 
   def test_valid_coordinate?
@@ -50,38 +29,22 @@ class CellTest < Minitest::Test
     assert_equal false, @board.valid_coordinate?("A22")
   end
 
-<<<<<<< HEAD
-  # maybe split into three tests and three methods
   def test_valid_placement?
-    # check to see if coordinate array is the length of the ship
-    # assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2"])
-    # assert_equal false,  @board.valid_placement?(@submarine, ["A2", "A3", "A4"])
-
-    # make sure the coordinates are consecutive
-    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2", "A4"])
-    # assert_equal false, @board.valid_placement?(@submarine, ["A1", "C1"])
-    # assert_equal false, @board.valid_placement?(@cruiser, ["A3", "A2", "A1"])
-    # assert_equal false, @board.valid_placement?(@submarine, ["C1", "B1"])
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2"])
+    assert_equal false, @board.valid_placement?(@submarine, ["A2", "A3", "A4"])
   end
 
-=======
-  def test_method_consecutive?
+  def test_consecutive?
+    # test placement is consecutive
     assert_equal false, @board.consecutive?(@cruiser, ["A1", "A2", "A4"])
+    assert_equal false, @board.consecutive?(@submarine, ["A1", "C1"])
+    assert_equal true, @board.consecutive?(@cruiser, ["A3", "A2", "A1"])
+    assert_equal true, @board.consecutive?(@submarine, ["C1", "B1"])
+    # test placement can't be diagnol
+    assert_equal false, @board.consecutive?(@submarine, ["A1", "B2"])
+    assert_equal false, @board.consecutive?(@cruiser, ["D1", "C2", "B3"])
+    assert_equal true, @board.consecutive?(@cruiser, ["D1", "D2", "D3"])
+    assert_equal true, @board.consecutive?(@submarine, ["A1", "A2"])
   end
 
-  # def test_valid_placement?
-    #length
-    # assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2"])
-    # assert_equal false, @board.valid_placement?(@submarine, ["A2", "A3", "A4"])
-    #consecutive
-    # assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2", "A4"])
-  # end
-
-  # def test_consecutive?
-  #   @board.valid_placement?(@cruiser, ["A1", "A2", "A4"])
-  #   @board.valid_placement?(@submarine, ["A1", "C1"])
-  #   @board.valid_placement?(@cruiser, ["A3", "A2", "A1"])
-  #   @board.valid_placement?(@submarine, ["C1", "B1"])
-  # end
->>>>>>> 35cb8c6f559f53aa3ca99e587760735abf3eea57
 end
