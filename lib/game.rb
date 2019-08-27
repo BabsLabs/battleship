c4e442eae0ac1c2ef83de7ef00cb85fe57fbb9c8
Brian Bower <brian@brianbower.com>
2019-08-27 11:23:28 -0600

    Add game and game_test framework - might not use
    Co-authored-by: David Barriga-Gomez <50917634+DavidBarriga-Gomez@users.noreply.github.com>

---
+++ lib/game.rb
@@ -0,0 +1,17 @@
+class Game
+
+  def start_game
+    welcome
+    play_prompt
+  end
+
+  def welcome
+    p "Welcome to BATTLESHIP!"
+  end
+
+  def play_prompt
+    p "Enter p to play. Enter q to quit."
+  end
+
+
+end
