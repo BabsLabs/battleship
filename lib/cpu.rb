71ecbff8e197c05250aff3c8a6dcc5787fbf31fc
Brian Bower <brian@brianbower.com>
2019-08-27 21:23:13 -0600

    Fix consecutive? method
    Co-authored-by: David Barriga-Gomez <50917634+DavidBarriga-Gomez@users.noreply.github.com>

--- lib/cpu.rb
+++ lib/cpu.rb
@@ -1,17 +1,16 @@
 class Cpu
   attr_reader :cpu_board
+
   def initialize(cpu_board)
     @cpu_board = Board.new
     @ships = [Ship.new("cruiser", 3), Ship.new("submarine", 2)]
     @cpu_cruiser = Ship.new("Cruiser", 3)
     @cpu_submarine = Ship.new("Submarine", 2)
-    binding.pry
   end
 
-  def place_ships
-    @ship.each do |ship|
-
-
+  def cpu_place_ships
+    @ships.each do |ship|
+      #something
+    end
   end
 end
-end 
--- lib/game.rb
+++ lib/game.rb
@@ -5,6 +5,7 @@ require './lib/ship'
 class Game
   def start_game
     puts "**** Welcome to BATTLESHIP! ****"
+    puts "Enter p to play. Enter q to quit."
       user_input = ""
       until user_input == "p" || user_input == "q"
         user_input = gets.chomp.downcase
@@ -15,5 +16,6 @@ class Game
         else
           puts "Invalid response. Enter valid response"
         end
-    end
+      end
   end
+end
