c342b9d57c5c7b847ba387af472313773a7c1392
Brian Bower <brian@brianbower.com>
2019-08-28 18:05:11 -0600

    Update incorrect prompt
    Co-authored-by: David Barriga-Gomez <50917634+DavidBarriga-Gomez@users.noreply.github.com>

--- lib/human.rb
+++ lib/human.rb
@@ -37,7 +37,7 @@ class Human
       @human_board.render(true)
       break if @human_board.valid_placement?(ship_to_place, human_input)
     else
-      puts "Invalid Input! Try again!"
+      puts "That is an invalid coordinat! Try again! Example: A1"
     end
     end
   end
